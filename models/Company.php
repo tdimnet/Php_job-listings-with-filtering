65bd1b8c515a477373149746f2e695d3347259c1
tdimnet <thomas.dimnet@gmail.com>
2024-10-30 11:10:47 +0100

    Creatng Company popo

---
+++ models/Company.php
@@ -0,0 +1,38 @@
+<?php
+
+class Company {
+    private int $id;
+    private string $name;
+    private string $img;
+
+    public function __construct(string $name, string $img)
+    {
+        $this->name = $name;
+        $this->img = $img;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getImg(): string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img)
+    {
+        $this->img = $img;
+    }
+}
