ccbb8d190830486867564e8825312f9965da608c
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-11-08 18:38:43 +0100

    Running sql statement and make it work

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 
 https://dev.to/fuadardiono/plain-old-php-object-usability-and-benefits-in-modern-php-programming-32oc
+https://www.geeksforgeeks.org/how-to-execute-an-sql-query-and-fetch-results-using-php/
+https://openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql/914293-accedez-aux-donnees-en-php-avec-pdo
+
--- index.php
+++ index.php
@@ -4,11 +4,20 @@ require_once("./vendor/autoload.php");
 require_once("./config/database.php");
 require_once("./models/JobOffer.php");
 
-var_dump($conn);
-
-
 $loader = new \Twig\Loader\FilesystemLoader("./views");
 $twig = new \Twig\Environment($loader);
+
+
+$sql = "SELECT * FROM tag";
+
+$sqlStatement = $conn->prepare($sql);
+$sqlStatement->execute();
+
+$data = $sqlStatement->fetchAll();
+
+var_dump($data);
+
+
 
 echo $twig->render("home.twig", [
     "name" => "Thomas"
