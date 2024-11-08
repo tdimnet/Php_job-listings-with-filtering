924d26183ade18e43c9f3bd902efcd5f92e8a5ff
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-11-08 18:30:03 +0100

    Update data files

--- config/data.sql
+++ config/data.sql
@@ -1,5 +1,3 @@
--- Adminer 4.8.1 MySQL 9.1.0 dump
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -10,13 +8,24 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `tag` varchar(255) COLLATE utf8mb4_0900_as_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_0900_as_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_ci;
 
-INSERT INTO `tag` (`id`, `tag`) VALUES
+INSERT INTO `tag` (`id`, `name`) VALUES
 (1,	'front'),
 (2,	'senior');
 
--- 2024-11-08 17:18:31
+
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE `company` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_0900_as_ci NOT NULL,
+  `img` varchar(255) COLLATE utf8mb4_0900_as_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_ci;
+
+INSERT INTO `company` (`id`, `name`, `img`) VALUES
+(1,	'photosnap', 'photosnap.png'),
+(2,	'manage', 'manage.png');
 
