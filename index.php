adfc82171c73f25355bf0b575ec697c4f5884436
tdimnet <thomas.dimnet@gmail.com>
2024-11-14 17:07:18 +0100

    Adding sql query statement

--- index.php
+++ index.php
@@ -8,9 +8,14 @@ $loader = new \Twig\Loader\FilesystemLoader("./views");
 $twig = new \Twig\Environment($loader);
 
 
-$mainQuery = "SELECT * FROM job_offer";
+// $mainQuery = "SELECT * FROM job_offer";
+$q = "
+    SELECT job_offer.id, job_offer.name, company.name, company.img
+    FROM job_offer
+    INNER JOIN company ON job_offer.company_id = company.id
+";
 
-$sqlStatement = $conn->prepare($mainQuery);
+$sqlStatement = $conn->prepare($q);
 $sqlStatement->execute();
 
 $jobOffersData = $sqlStatement->fetchAll();
