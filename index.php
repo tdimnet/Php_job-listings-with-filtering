1473512bff9ce362e0a29ca9258d00f2ea1c1bb8
tdimnet <thomas.dimnet@gmail.com>
2024-11-28 19:36:33 +0100

    Adding multiple join on sql statement

--- index.php
+++ index.php
@@ -10,13 +10,17 @@ $twig = new \Twig\Environment($loader);
 
 // $mainQuery = "SELECT * FROM job_offer";
 $q = "
-    SELECT 
+SELECT 
         job_offer.id AS job_offer_id, 
         job_offer.name AS job_title, 
         company.name AS company_name,
-        company.img as company_img
+        company.img AS company_img,
+        GROUP_CONCAT(tag.name SEPARATOR ', ') AS tags
     FROM job_offer
     INNER JOIN company ON job_offer.company_id = company.id
+    LEFT JOIN job_offer_tags ON job_offer.id = job_offer_tags.job_offer_id
+    LEFT JOIN tag ON job_offer_tags.tag_id = tag.id
+    GROUP BY job_offer.id, job_offer.name, company.name, company.img;
 ";
 
 $sqlStatement = $conn->prepare($q);
